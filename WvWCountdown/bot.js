67be66b43840ab6209eb23b654068a0d0ee3e10a
Darrin Olsen <dolsen@cmnhospitals.org>
2023-01-04 16:20:10 -0700

    updated node v18.12.1 & discord.js v14

--- WvWCountdown/bot.js
+++ WvWCountdown/bot.js
@@ -1,30 +1,33 @@
-const Discord = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const auth = require('./auth.json');
+const url = `https://api.guildwars2.com/v2/wvw/matches/${auth.region}-1?access_token=${auth.gw2token}`;
+
 const countdown = require('countdown');
 const request = require('request');
-var auth = require('./auth.json');
-const client = new Discord.Client();
-var url = 'https://api.guildwars2.com/v2/wvw/matches/1-1?access_token=' + auth.gw2token;
+
 
 function getApi(){
-    console.log(request.get(url, { json: true }, (err, res, body) => {
+   request.get(url, { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         global.vEndTime = body.end_time;
         console.log(global.vEndTime);
-    }));
+    0});
 };
 
 client.on('ready', () => {
     getApi();
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`WvW Ends at: ${global.vEndTime}`);
-    console.log(countdown(new Date(),new Date(global.vEndTime), countdown.DAYS | countdown.HOURS | countdown.MINUTES).toString());
+
     client.user.setStatus('available');
     client.user.setPresence({
         game: {
             name: "Connecting to API..."
         }
     });
-    console.log();
+    
     setInterval(() => {
         getApi();
         var datetime = countdown(new Date(),new Date(global.vEndTime), countdown.DAYS | countdown.HOURS | countdown.MINUTES).toString();
@@ -32,4 +35,5 @@ client.on('ready', () => {
         client.user.setActivity(datetime);
     }, 60000)
 });
+
 client.login(auth.token);
