d7f5babb6fb0c87f73122e151c340950acf7e47b
Darrin Olsen <dolsen@cmnhospitals.org>
2023-01-14 12:06:44 -0700

    removed request, using https

--- WvWCountdown/bot.js
+++ WvWCountdown/bot.js
@@ -1,43 +1,44 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const https = require('https');
 
 const auth = require('./auth.json');
 const url = `https://api.guildwars2.com/v2/wvw/matches/${auth.region}-1?access_token=${auth.gw2token}`;
 
-const request = require('request');
-
-
-function getApi(){
-   request.get(url, { json: true }, (err, res, body) => {
-        if (err) { return console.log(err); }
-        global.vEndTime = body.end_time;
-        // console.log(global.vEndTime);
-    0});
+function getApi(){	
+	https.get(url, (res) => {
+		let data = '';
+		res.on('data', (chunk) => {
+			data += chunk;
+		});
+		res.on('end', () => {
+			const responseData = JSON.parse(data);
+			// console.log(responseData.end_time);
+			global.vEndTime = responseData.end_time
+		});
+	  });
 };
 
 client.on('ready', () => {
-    getApi();
-    // console.log(`Logged in as ${client.user.tag}!`);
-    // console.log(`WvW Ends at: ${global.vEndTime}`);
-
-    client.user.setStatus('available');
-    client.user.setPresence({
-        game: {
-            name: "Connecting to API..."
-        }
-    });
-    
-    setInterval(() => {
-        getApi();
-        let currentDate = new Date();
-        let dateToCompare = Date.parse(global.vEndTime);
-        let diff = dateToCompare - currentDate;
-        let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        // console.log(`${days} days, ${hours} hours, ${minutes} minutes`);
-        client.user.setActivity(`${days} days, ${hours} hours, ${minutes} minutes`);
-    }, 60000)
+	getApi();
+	client.user.setStatus('available');
+	client.user.setPresence({
+		game: {
+			name: "Connecting to API..."
+		}
+	});
+	
+	setInterval(() => {
+		getApi();
+		let currentDate = new Date();
+		let dateToCompare = Date.parse(global.vEndTime);
+		let diff = dateToCompare - currentDate;
+		let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+		let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+		// console.log(`${days} days, ${hours} hours, ${minutes} minutes`);
+		client.user.setStatus(`${days} days, ${hours} hours, ${minutes} minutes`);
+	}, 1000)
 });
 
 client.login(auth.token);
