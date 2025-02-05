3cea910ec48c8b64c68f8efbf9725a8fef002b16
Azhkanizkael <darrin.olsen@ymail.com>
2025-02-05 15:42:00 -0700

    Fixed commands

--- WvWCountdown/commands/tools/lockout.js
+++ WvWCountdown/commands/tools/lockout.js
@@ -1,31 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { getLockoutTimer } = require('../../functions/functions.js')
 
-
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('lockout')
 		.setDescription('Returns time until Lockout'),
 	async execute(interaction) {
-		getLockoutTimer((error, Value) => {
-			if (error) {
-				console.log('Error:', error);
-			} else {
-				//console.log('Lockout Timer Value:', Value);
-				let LockOutDate = Value
-				let currentDate = new Date();
-				let dateToCompare = Date.parse(LockOutDate);
-				let diff = dateToCompare - currentDate;
-				let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-				let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-				let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-				let status = `${days} days, ${hours} hours, ${minutes} minutes`
-				console.log(`Lockout in: ${status}`);
-				//client.user.setActivity(status);
-				global.lockout = `Lockout in: ${status}`;
-			}
-		});
+        const lockoutTime = await getLockoutTimer();
+        let currentDate = new Date();
+        let dateToCompare = Date.parse(lockoutTime);
+        let diff = dateToCompare - currentDate;
+        let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        let status = `${days} days, ${hours} hours, ${minutes} minutes`;
+        console.log(`Lockout in: ${status}`);
+        global.lockout = `Lockout in: ${status}`;
+        global.lockoutdays = days;
+        global.lockouthours = hours;
+        global.lockoutminutes = minutes;
         const message = await interaction.deferReply({
 			fetchReply: true,
 		});
